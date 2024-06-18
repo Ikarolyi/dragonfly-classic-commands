83ffc3baed39ae6f116732f00aab63b56fad8c47
István Károlyi <karolyi.istvan1234@gmail.com>
2024-06-18 17:27:26 +0200

    /teleport

--- Init.go
+++ Init.go
@@ -12,6 +12,7 @@ func Init() {
 	cmd.Register(cmd.New("setblock", "...", []string{}, commands.Setblock{}))
 	cmd.Register(cmd.New("list", "Lists the players on the server", []string{}, commands.ListPlayers{}))
 	cmd.Register(cmd.New("tell", "Sends a private message to one or more players", []string{"msg", "w"}, commands.Whisper{}))
+	cmd.Register(cmd.New("teleport", "Teleports players", []string{"tp"}, commands.Teleport{}))
 }
 
 func PassAccept(p *player.Player){
---
+++ commands/teleport.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"github.com/Ikarolyi/dragonfly-classic-commands/utils"
+	"github.com/df-mc/dragonfly/server/cmd"
+	"github.com/df-mc/dragonfly/server/player"
+	"github.com/go-gl/mathgl/mgl64"
+)
+
+type Teleport struct {
+	Victim cmd.Optional[[]cmd.Target]
+	Target cmd.Optional[[]cmd.Target]
+	Destination cmd.Optional[mgl64.Vec3]
+}
+
+func (c Teleport) Run(source cmd.Source, output *cmd.Output) {
+	sender := utils.GetSender(source)
+
+	victimTarget, victimSet := c.Victim.Load()
+	var victim *player.Player
+	if !victimSet{
+		victim = sender
+	}else{
+		victim, _ = victimTarget[0].(*player.Player)
+	}
+
+	var destination mgl64.Vec3
+	destination, desdestinationSet := c.Destination.Load()
+
+
+	target, targetSet := c.Target.Load()
+	if !targetSet && !desdestinationSet{
+		destination = sender.Position()
+	}else if !desdestinationSet{
+		targetP, _ := target[0].(* player.Player)
+		destination = targetP.Position()
+	}
+
+	victim.Teleport(destination)
+}
